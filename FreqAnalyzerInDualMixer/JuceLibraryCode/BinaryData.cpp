808e1f17eded073012bfc26e8d65daadd981d6dc
Louis Deng <boyu.deng@mail.mcgill.ca>
2024-10-01 16:29:41 -0400

    Initial Commit

---
+++ FreqAnalyzerInDualMixer/JuceLibraryCode/BinaryData.cpp
@@ -0,0 +1,58 @@
+/* ==================================== JUCER_BINARY_RESOURCE ====================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#include <cstring>
+
+namespace BinaryData
+{
+
+//================== Notes.txt ==================
+static const unsigned char temp_binary_data_0[] =
+"";
+
+const char* Notes_txt = (const char*) temp_binary_data_0;
+
+
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
+{
+    unsigned int hash = 0;
+
+    if (resourceNameUTF8 != nullptr)
+        while (*resourceNameUTF8 != 0)
+            hash = 31 * hash + (unsigned int) *resourceNameUTF8++;
+
+    switch (hash)
+    {
+        case 0xe2aa17d2:  numBytes = 0; return Notes_txt;
+        default: break;
+    }
+
+    numBytes = 0;
+    return nullptr;
+}
+
+const char* namedResourceList[] =
+{
+    "Notes_txt"
+};
+
+const char* originalFilenames[] =
+{
+    "Notes.txt"
+};
+
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
+{
+    for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
+            return originalFilenames[i];
+
+    return nullptr;
+}
+
+}
---
+++ FreqAnalyzerInDualMixer/Source/PluginProcessor.cpp
@@ -0,0 +1,208 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+FreqAnalyzerInDualMixerAudioProcessor::FreqAnalyzerInDualMixerAudioProcessor()
+: mBufferSize(0)
+, mSampleRate(0.0)
+, parameters (*this, nullptr, juce::Identifier ("PVT"),{
+    
+})
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    // initialzing all the unique_ptr_s
+    for (int i=0;i<2;i++)
+    {
+        DWM[i].reset( new DWmixer<float> );
+    }
+}
+
+FreqAnalyzerInDualMixerAudioProcessor::~FreqAnalyzerInDualMixerAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String FreqAnalyzerInDualMixerAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool FreqAnalyzerInDualMixerAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool FreqAnalyzerInDualMixerAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool FreqAnalyzerInDualMixerAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double FreqAnalyzerInDualMixerAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int FreqAnalyzerInDualMixerAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int FreqAnalyzerInDualMixerAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void FreqAnalyzerInDualMixerAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String FreqAnalyzerInDualMixerAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void FreqAnalyzerInDualMixerAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void FreqAnalyzerInDualMixerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void FreqAnalyzerInDualMixerAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool FreqAnalyzerInDualMixerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void FreqAnalyzerInDualMixerAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        // ..do something to the data...
+    }
+}
+
+//==============================================================================
+bool FreqAnalyzerInDualMixerAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* FreqAnalyzerInDualMixerAudioProcessor::createEditor()
+{
+    return new FreqAnalyzerInDualMixerAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void FreqAnalyzerInDualMixerAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    auto state = parameters.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    copyXmlToBinary (*xml, destData);
+    //DBG("SAVED STATE INFO IN" << "..." << "! ");
+}
+
+void FreqAnalyzerInDualMixerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+     
+    if (xmlState.get() != nullptr)
+        if (xmlState->hasTagName (parameters.state.getType()))
+        {
+            parameters.replaceState (juce::ValueTree::fromXml (*xmlState));
+            //DBG("GOT STATE INFO FROM " << "..." << "! ");
+        }
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new FreqAnalyzerInDualMixerAudioProcessor();
+}
